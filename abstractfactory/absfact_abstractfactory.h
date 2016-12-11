4bf8604b7219abac3a8f5f5faa46ae7f70b5ba5e
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2016-12-11 17:51:17 +0200

    Add Abstract Factory description

--- abstractfactory/absfact_abstractfactory.h
+++ abstractfactory/absfact_abstractfactory.h
@@ -7,6 +7,27 @@
 
 using namespace ABSFACT;
 
+/*!
+ * What is the Abstract Factory Pattern
+ *
+ * It is like a factory, but everything is encapsulated
+ * The method that orders the object
+ * The factories that build the object
+ * The final objects
+ * The final objects contain objects that use Strategy Pattern
+ *  Composition: Object class fields are objects
+ *
+ * What can you do with an Abstract Factory
+ *
+ * Allows you to create families of related objects without
+ * specifying a concrete class
+ * Use when you have many objects that can be added, or changed
+ * dynamically during runtime
+ * You can model anything you can imagine and have those objects
+ * interact though common interfaces
+ * The Bad: Things can get complicated
+ */
+
 void abstractfactorypattern()
 {
     auto makeUFOs = std::make_unique<UFOEnemyShipBuilding>();
